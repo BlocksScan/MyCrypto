90185bd32142d8f8783c1206b75f6821b7681b39
michael blurpesec <hahn.michael.f@gmail.com>
2021-12-16 14:57:50 -0500

    fixed ts errors. working - afaik

--- src/components/TransactionFlow/TxReceipt.tsx
+++ src/components/TransactionFlow/TxReceipt.tsx
@@ -42,7 +42,13 @@ import {
   TxQueryTypes,
   WalletId
 } from '@types';
-import { bigNumValueToViewableEther, buildTxUrl, isType2Receipt, isWeb3Wallet, truncate } from '@utils';
+import {
+  bigNumValueToViewableEther,
+  buildTxUrl,
+  isType2Receipt,
+  isWeb3Wallet,
+  truncate
+} from '@utils';
 import { constructCancelTxQuery, constructSpeedUpTxQuery } from '@utils/queries';
 import { path } from '@vendor';
 
@@ -59,7 +65,6 @@ import { TxReceiptTotals } from './TxReceiptTotals';
 import { ISender } from './types';
 
 import './TxReceipt.scss';
-
 
 interface PendingBtnAction {
   text: string;
@@ -120,7 +125,7 @@ const TxReceipt = ({
   );
 
   const displayTxReceipt = receipt ?? userTx!; // @todo: figure out why this was swapped
-  console.debug('displayTxReceipt', txReceipt)
+  console.debug('displayTxReceipt', txReceipt);
   const timestamp = displayTxReceipt.timestamp ?? 0;
   const txStatus = displayTxReceipt.status ?? 0;
 
@@ -171,18 +176,17 @@ const TxReceipt = ({
     });
   }, []);
 
-  const baseAssetRate = getAssetRate(txConfig.baseAsset)
+  const baseAssetRate = getAssetRate(txConfig.baseAsset);
   const valueTransfers = (() => {
-
-    if (!displayTxReceipt) return []
-    const valueTransferEvents = displayTxReceipt.erc20Transfers.map((transfer) => ({
+    if (!displayTxReceipt) return [];
+    const valueTransferEvents = displayTxReceipt.valueTransfers.map((transfer) => ({
       ...transfer,
       toContact: getContactByAddressAndNetworkId(transfer.to, network.id),
       fromContact: getContactByAddressAndNetworkId(transfer.from, network.id),
       rate: getAssetRate(transfer.asset),
       amount: transfer.amount
-    }))
-    console.debug('here: transfers ', displayTxReceipt.erc20Transfers, valueTransferEvents)
+    }));
+    console.debug('here: transfers ', displayTxReceipt.valueTransfers, valueTransferEvents);
     if (displayTxReceipt && !displayTxReceipt.value.isZero()) {
       valueTransferEvents.push({
         to: displayTxReceipt.to,
@@ -192,19 +196,21 @@ const TxReceipt = ({
         amount: bigNumValueToViewableEther(displayTxReceipt.value),
         asset: displayTxReceipt.baseAsset,
         rate: baseAssetRate
-      })
+      });
     } else if (txConfig.amount != '0') {
       valueTransferEvents.push({
         to: txConfig.receiverAddress || txConfig.rawTransaction.to!, // @todo: remove
         from: txConfig.senderAccount.address,
-        toContact: txConfig.receiverAddress && getContactByAddressAndNetworkId(txConfig.receiverAddress, network.id),
+        toContact:
+          txConfig.receiverAddress &&
+          getContactByAddressAndNetworkId(txConfig.receiverAddress, network.id),
         fromContact: getContactByAddressAndNetworkId(txConfig.senderAccount.address, network.id),
         amount: txConfig.amount,
         asset: txConfig.baseAsset,
         rate: baseAssetRate
-      })
+      });
     }
-    return valueTransferEvents
+    return valueTransferEvents;
   })();
 
   const handleTxSpeedUpRedirect = async () => {
@@ -254,7 +260,7 @@ const TxReceipt = ({
       />
     );
   }
-  console.debug('valueTransfers', valueTransfers)
+  console.debug('valueTransfers', valueTransfers);
   return (
     <TxReceiptUI
       settings={settings}
@@ -310,7 +316,7 @@ export interface TxReceiptDataProps {
   protectTxButton?(): JSX.Element;
   customComponent?(): JSX.Element;
   network: Network;
-  valueTransfers: ITxTransferEvent[]
+  valueTransfers: ITxTransferEvent[];
 }
 
 type UIProps = Omit<IStepComponentProps, 'resetFlow' | 'onComplete'> & TxReceiptDataProps;
